c3080ab1098e3972c19c4311bede80a5f64d20f3
Unknown <lorenzo.palumbo@hotmail.com>
2020-04-08 13:02:56 +0200

    Milestone 1.1

--- js/main.js
+++ js/main.js
@@ -9,43 +9,44 @@ $(document).ready(function(){
 
             for (var i = 0; i < data.length; i++) {
                 var oggettoSingolo = data[i];
-                var meseVendita = oggettoSingolo.date;
-                // moment(meseVendita).month();
-                // console.log(moment(meseVendita).month());
-                console.log(meseVendita);
+                var dataVendita = oggettoSingolo.date;
+                var dataVendita = moment(oggettoSingolo.date, 'DD/MM/YYYY');
+                var meseVendita = dataVendita.locale('it').format('MMMM');
+                // console.log(meseVendita);
                 if (oggettoIntermedio[meseVendita] === undefined) {
                     oggettoIntermedio[meseVendita] = 0
                 }
                 oggettoIntermedio[meseVendita] += oggettoSingolo.amount;
-                // console.log(oggettoIntermedio);
+                console.log(oggettoIntermedio);
             };
+
+            var mesi = [];
+            var totaleVendita = [];
+
+            for (var key in oggettoIntermedio) {
+                mesi.push(key);
+                totaleVendita.push(oggettoIntermedio[key]);
+            }
+
+                var ctx = $('#myChart');
+                var chart = new Chart(ctx, {
+
+                    type: 'line',
+
+                    data: {
+                        labels: mesi,
+                        datasets: [{
+                            label: 'Vendite totali anno 2017',
+                            // backgroundColor: 'rgb(255, 99, 132)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            data: totaleVendita
+                        }]
+                    },
+                });
         },
         error: function(){
             alert('errore');
         }
     });
-
-
-
-    // var ctx = $('#myChart');
-    // var chart = new Chart(ctx, {
-    //
-    //     type: 'line',
-    //
-    //     data: {
-    //         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    //         datasets: [{
-    //             label: 'My First dataset',
-    //             // backgroundColor: 'rgb(255, 99, 132)',
-    //             borderColor: 'rgb(255, 99, 132)',
-    //             data: [0, 10, 5, 2, 20, 30, 45]
-    //         }]
-    //     },
-    // });
-
-
-
-
-
 
 });
