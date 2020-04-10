690175e8409b62fda21725e7301ee4c765ecfa4b
Unknown <lorenzo.palumbo@hotmail.com>
2020-04-10 14:48:26 +0200

    Milestone 1.2 e Milestone 2

--- js/main.js
+++ js/main.js
@@ -1,58 +1,109 @@
 $(document).ready(function(){
 
-    get();
+    stampaGrafici();
     $('.invio').click(function(){
         var nomeSelezionato = $('#salesman').val();
         var valoreInserito = parseInt($('.valore').val());
         var dataDaModificare = $('#data').val();
-        var data = moment(dataDaModificare).locale('it').format('DD/MM/YYYY');
+        var data = moment(dataDaModificare, 'YYYY-MM-DD').format('DD/MM/YYYY');
         post(nomeSelezionato, valoreInserito, data);
-        get();
+        stampaGrafici();
     });
 
-    function get(){
+
+
+    function costruttoreDatiMensili(data){
+        var oggettoIntermedio = {};
+
+        for (var i = 0; i < data.length; i++) {
+            var oggettoSingolo = data[i];
+            var dataVendita = oggettoSingolo.date;
+            var meseVendita = moment(dataVendita, 'DD/MM/YYYY').format('MMMM');
+            // console.log(meseVendita);
+            if (oggettoIntermedio[meseVendita] === undefined) {
+                oggettoIntermedio[meseVendita] = 0
+            }
+            oggettoIntermedio[meseVendita] += parseInt(oggettoSingolo.amount);
+        };
+
+        var mesi = [];
+        var totaleVendita = [];
+
+        for (var key in oggettoIntermedio) {
+            mesi.push(key);
+            totaleVendita.push(oggettoIntermedio[key]);
+        }
+        return{
+            mesi: mesi,
+            vendite: totaleVendita
+        };
+    };
+
+    function graficoLine(mesi, totaleVendita){
+
+        var ctx = $('#myLineChart');
+        var chart = new Chart(ctx, {
+
+            type: 'line',
+
+            data: {
+                labels: mesi,
+                datasets: [{
+                    label: 'Vendite totali anno 2017',
+                    backgroundColor: 'rgba(255, 99, 132, 0.58)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: totaleVendita
+                }]
+            },
+        });
+    };
+
+    function costruttoreDatiVenditori(data){
+        var venditeVenditori ={};
+        for (var i = 0; i < data.length; i++) {
+            var oggettoSingolo = data[i];
+            var venditore = oggettoSingolo.salesman;
+            if (venditeVenditori[venditore] === undefined) {
+                venditeVenditori[venditore] = 0;
+            }
+            venditeVenditori[venditore] += parseInt(oggettoSingolo.amount);
+        }
+        var arrayVenditori = [];
+        var arrayTotVenditore = [];
+        for (var key in venditeVenditori) {
+            arrayVenditori.push(key);
+            arrayTotVenditore.push(venditeVenditori[key]);
+        }
+        return{
+            venditori: arrayVenditori,
+            vendite: arrayTotVenditore
+        }
+    };
+
+    function graficoDoughnut(venditori, totVenditore){
+        var ctx = $('#myDoughnutChart');
+        var chart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: totVenditore,
+                    backgroundColor: ['#ed5555', '#6ca36c', '#6565b8', '#ae5e5e']
+                }],
+
+                labels: venditori
+            }
+        });
+    };
+
+    function stampaGrafici(){
         $.ajax({
             url: "http://157.230.17.132:4025/sales",
             method: "GET",
             success: function (data){
-                // console.log(data);
-                var oggettoIntermedio = {};
-
-                for (var i = 0; i < data.length; i++) {
-                    var oggettoSingolo = data[i];
-                    var dataVendita = oggettoSingolo.date;
-                    var dataVendita = moment(oggettoSingolo.date, 'DD/MM/YYYY');
-                    var meseVendita = dataVendita.locale('it').format('MMMM');
-                    // console.log(meseVendita);
-                    if (oggettoIntermedio[meseVendita] === undefined) {
-                        oggettoIntermedio[meseVendita] = 0
-                    }
-                    oggettoIntermedio[meseVendita] += oggettoSingolo.amount;
-                };
-
-                var mesi = [];
-                var totaleVendita = [];
-
-                for (var key in oggettoIntermedio) {
-                    mesi.push(key);
-                    totaleVendita.push(oggettoIntermedio[key]);
-                }
-
-                    var ctx = $('#myChart');
-                    var chart = new Chart(ctx, {
-
-                        type: 'line',
-
-                        data: {
-                            labels: mesi,
-                            datasets: [{
-                                label: 'Vendite totali anno 2017',
-                                // backgroundColor: 'rgb(255, 99, 132)',
-                                borderColor: 'rgb(255, 99, 132)',
-                                data: totaleVendita
-                            }]
-                        },
-                    });
+                var datiMensili = costruttoreDatiMensili(data);
+                graficoLine(datiMensili.mesi, datiMensili.vendite);
+                var datiVenditori = costruttoreDatiVenditori(data);
+                graficoDoughnut(datiVenditori.venditori, datiVenditori.vendite);
             },
             error: function(){
                 alert('errore');
@@ -70,11 +121,24 @@ $(document).ready(function(){
                 date: data
             },
             success: function(data){
-                console.log(data);
+                graficoLine();
             },
             error: function(){
                 alert('errore');
             }
         });
     };
+
+    // $.ajax({
+    //     url: 'http://157.230.17.132:4025/sales/60',
+    //     method: 'DELETE',
+    //     success: function(data){
+    //        stampaGrafici();
+    //     },
+    //     error: function(){
+    //         alert('errore');
+    //     }
+    // });
+
+
 });
